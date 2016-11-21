e5854b0df9dbc56345b751f1fcedc6cf8817fd7e
philipp <philipp.b@ya.ru>
2016-11-21 19:18:19 +0300

    first version. simply grab urls and check them

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var pump = require('pump');
 
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('build', function (cb) {
+gulp.task('js', function (cb) {
 	pump([
 			gulp.src([
 				'bower_components/google-page-speed-scores/google-page-speed-scores.package.min.js',
-				'src/js/fetch-all.js'
+				'src/js/*.js'
 			]),
 			uglify(),
 			concat('app.min.js'),
@@ -17,7 +18,22 @@ gulp.task('build', function (cb) {
 		cb
 	);
 });
+gulp.task('css', function (cb) {
+	pump([
+			gulp.src([
+				'bower_components/skeleton/css/normalize.css',
+				'bower_components/skeleton/css/skeleton.css',
+				'src/css/*.css'
+			]),
+			cleanCSS(),
+			concat('app.min.css'),
+			gulp.dest('assets/css')
+		],
+		cb
+	);
+});
 
 gulp.task('default', [
-	'build'
+	'js',
+	'css'
 ]);
